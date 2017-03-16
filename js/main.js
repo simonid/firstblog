e5893140b9d842f53a45ae40c3ac9d721e31bbe5
simon <simon@woshisimonchen@gmail.com>
2017-03-16 13:47:53 +0800

    Site updated: 2017-03-16 13:47:52

--- js/main.js
+++ js/main.js
@@ -49,6 +49,43 @@ var BackPic = (function(){
 	}
 })();
 
+/*
+var search = function(){
+    require(['./search.js'], function(){
+        var inputArea = document.querySelector("#st-search-input");
+        var $HideWhenSearch = $("#toc, #tocButton, .post-list, #post-nav-button a:nth-child(2)");
+        var $resultArea = $("#local-search-result");
+
+        var getSearchFile = function(){
+            var search_path = "/search.xml";
+            var path = search_path;
+            searchFunc(path, 'st-search-input', 'local-search-result');
+        }
+
+        var getFileOnload = inputArea.getAttribute('searchonload');
+        if (getFileOnload === "true") {
+            getSearchFile();
+        } else {
+            inputArea.onfocus = function(){ getSearchFile() }
+        }
+
+        var HideTocArea = function(){
+            $HideWhenSearch.css("visibility","hidden");
+        }
+        inputArea.oninput = function(){ HideTocArea() }
+        inputArea.onkeydown = function(){ if(event.keyCode==13) return false}
+
+        $resultArea.bind("DOMNodeRemoved DOMNodeInserted", function(e) {
+            if (!$(e.target).text()) {
+                $(".no-result").show(200);
+            } else {
+              $(".no-result").hide();
+            }
+        })
+    })
+}();
+*/
+
 var Search=(function(){
 	var searchFunc = function(path, search_id, content_id) {
     'use strict';
@@ -264,6 +301,7 @@ var Main = (function(){
 			Tips.init();
 			BackPic.init();
 			Search.init();
+			//serach.init();
 			new Mobile({
 				ctn: document.getElementsByClassName("slider-trigger")[0]
 			});
